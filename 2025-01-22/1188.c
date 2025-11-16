9acd642d00ab68daccabfbeab950d1a372cfde76
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-11-16 23:37:44 +0600

    Upload C solution for Beecrowd 1187 to 1190

---
+++ 2025-01-22/1188.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    double M[12][12],sum;
+    int i,j,count;
+    char O;
+    scanf("%c",&O);
+
+    for(i=0; i<12; i++)
+    {
+        for(j=0; j<12; j++)
+        {
+            scanf("%lf",&M[i][j]);
+        }
+    }
+
+    sum=count=0;
+    for(i=11; i>6; i--)
+    {
+        for(j=i-1; j>12-i; j--)
+        {
+            sum+=M[i][j];
+            count++;
+        }
+    }
+
+    if(O=='S')
+    {
+        printf("%.1lf\n",sum);
+    }
+    else if(O=='M')
+    {
+        printf("%.1lf\n",sum/count);
+    }
+
+
+
+
+    return 0;
+}
---
+++ 2025-01-22/1189.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+{
+    double M[12][12],sum;
+    int i,j,count;
+    char O;
+    scanf("%c",&O);
+
+    for(i=0; i<12; i++)
+    {
+        for(j=0; j<12; j++)
+        {
+            scanf("%lf",&M[i][j]);
+        }
+    }
+
+    sum=count=0;
+    for(i=0; i<5; i++)
+    {
+        for(j=1+i; j<11-i; j++)
+        {
+            sum+=M[j][i];
+            count++;
+        }
+    }
+    if(O=='S')
+    {
+        printf("%.1lf\n",sum);
+    }
+    else if(O=='M')
+    {
+        printf("%.1lf\n",sum/count);
+    }
+
+
+
+
+
+
+    return 0;
+}
